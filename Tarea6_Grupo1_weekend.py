31ec36e50a1b1d595f9d6667a38e5042d47c8df8
jakebd7 <ezerbd20@gmail.com>
2021-12-09 09:41:10 -0600

    update3

--- Tarea6_Grupo1_weekend.py
+++ Tarea6_Grupo1_weekend.py
@@ -1,1 +1,15 @@
 __autor__ = 'David Weeber, Jason Ortiz, Jekson Pineda, Amilcar Ibarra, Leonardo Gonzalez'
+
+from person import Person
+from tuition import Tuition
+#from course import Course
+
+print("\n         Welcome to the Academic Control System of the Oxford University\n")
+
+
+student1 = Person.create_person("Student")
+student1.name = "Juan"
+student1.last_name = "Rodriguez"
+
+tuition_student1 = Tuition()
+
