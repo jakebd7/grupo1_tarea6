d0d161415a74f37ed7994e7b4dae0b8e0f3a0c67
JeksonPineda <jecksonpineda@gmail.com>
2021-12-10 14:40:15 -0600

    Asignando a un profesor por curso

--- Datos.py
+++ Datos.py
@@ -1,3 +1,4 @@
+from Profesor import *
 import datetime
 import time
 from Matricula import Matricula
@@ -22,25 +23,33 @@ estudiante_min_programa = int(input("Ingrese Cuantos es el Minimo de Programas p
 # min_estudiante_para_abrir_curso = int(input("Ingrese el minimo de estudiantes para abrir un curso: "))
 # max_estudiante_para_abrir_curso = int(input("Ingrese el maximo de estudiantes para abrir un curso: "))
 
+"""Agregando Profesores y sus Cursos"""
 
-"""Programa y Curso 1"""
+
+"""Programa, Profesor y Curso 1"""
 programa_leyes = Programa("Auditoria en Leyes", time.ctime(), "Activo", "Juan Guarnizo")
+profesor_curso_leyes = Profesor("Mario Jose", "Dolores Estrada", "001-121212-1225L", "Masatepe", 88997744,
+                                "12 Dic 2012","josedolroes@gmail.com", "Planta", "Matutino")
 curso_leyes_1 = Curso("Leyes 1", 3, 40, 150, 90)
 curso_leyes_2 = Curso("Leyes 2", 3, 40, 150, 90)
 curso_leyes_3 = Curso("Leyes 3", 3, 40, 150, 90)
 curso_leyes_4 = Curso("Leyes 4", 3, 40, 150, 90)
 curso_leyes_final = Curso("Leyes Final", 1, 40, 150, 90)
 
-"""Programa y Curso 2"""
+"""Programa, Profesor y Curso 2"""
 programa_python = Programa("Programacion Python", time.ctime(), "Activo", "Luis Alguera")
+profesor_curso_python = Profesor("Luisa Marbelly", "Ramirez Gollena", "001-121212-1225L", "Masatepe", 88997744,
+                                 "12 Dic 2012", "luisagollena@gmail.com", "Planta", "Matutino")
 curso_python_1 = Curso("Python 1", 3, 45, 180, 95)
 curso_python_2 = Curso("Python 2", 3, 45, 180, 95)
 curso_python_3 = Curso("Python 3", 3, 45, 180, 95)
 curso_python_4 = Curso("Python 4", 3, 45, 180, 95)
 curso_python_final = Curso("Python Final", 1, 45, 180, 95)
 
-"""Programa y Curso 3"""
+"""Programa, Profesor y Curso 3"""
 programa_java = Programa("Programacion Java", time.ctime(), "Activo", "Mario Sanches")
+profesor_curso_java = Profesor("Francisco Mora", "Gutierre Urbina", "001-121212-1225L", "Masatepe", 88997744,
+                               "12 Dic 2012","franurbina@gmail.com", "Parcial", "Matutino")
 curso_java_1 = Curso("Java 1", 3, 45, 180, 95)
 curso_java_2 = Curso("Java 2", 3, 45, 180, 95)
 curso_java_3 = Curso("Java 3", 3, 45, 180, 95)
@@ -63,13 +72,18 @@ matricula3 = Matricula(time.ctime(), time.ctime(), "Programacion Java")
 
 if cursos_min_programa <= Programa.id_programa <= cursos_max_programa:
     if estudiante_min_programa <= Estudiante.id <= estudiante_max_programa:
-        print(matricula1, estudiante1, programa_leyes, curso_leyes_1, curso_leyes_2)  # Insercion 1
-        print(matricula2, estudiante2, programa_python, curso_python_1, curso_python_2)  # Insercion 2
-        print(matricula3, estudiante3, programa_java, curso_java_1, curso_java_2)  # Insercion 3
+        print(matricula1, estudiante1, programa_leyes, curso_leyes_1, curso_leyes_2,
+              profesor_curso_leyes)  # Insercion 1
+        print(matricula2, estudiante2, programa_python, curso_python_1, curso_python_2,
+              profesor_curso_python)  # Insercion 2
+        print(matricula3, estudiante3, programa_java, curso_java_1, curso_java_2, profesor_curso_java)  # Insercion 3
 
-        print(matricula1, estudiante1, programa_python, curso_python_1, curso_python_2)  # Insercion 4
-        print(matricula1, estudiante1, programa_java, curso_java_1, curso_java_2)  # Insercion 5
+        print(matricula1, estudiante1, programa_python, curso_python_1, curso_python_2, programa_python,
+              profesor_curso_python)  # Insercion 4
+        print(matricula1, estudiante1, programa_java, curso_java_1, curso_java_2, profesor_curso_java)  # Insercion 5
     else:
         print("Error en la cantidad de Programas para un Estudiuante")
 else:
     print("Error en la cantidad de programas Registrados")
+
+
